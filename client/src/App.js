70ecf9b894e3f22b1d19bdd718604852f9f49c0c
Rodrigo Oliveira <frejat.rodrigo@gmail.com>
2021-04-20 13:05:31 -0300

    primeira versao

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Navbar from './navbar/Navbar'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+        <h1>Ola meu cliente</h1>
+    </div>
+  );
+}
+
+export default App;
