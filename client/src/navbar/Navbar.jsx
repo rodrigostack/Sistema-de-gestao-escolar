92f7ac3d92c88a7910e6369d78f7bce12d41a378
Rodrigo Oliveira <frejat.rodrigo@gmail.com>
2021-04-20 19:48:21 -0300

    sexta versao

--- client/src/navbar/Navbar.jsx
+++ client/src/navbar/Navbar.jsx
@@ -18,7 +18,7 @@ export default function(){
           <Link className="nav-link" aria-current="page" to="/home">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Quem Somos?</a>
+          <Link className="nav-link" to="/quemsomos">Quem Somos?</Link>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">Contatos</a>
--- client/src/rotas/routes.js
+++ client/src/rotas/routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from '../home/Home'
+import Quemsomos from '../quemsomos/Quemsomos'
 
 export default function(){
     return(
       
         <Switch>
             <Route exact path="/home" component={Home}/>
+            <Route exact path="/quemsomos" component={Quemsomos}/>
         </Switch>
     
     )
